e6a31dde4f3602b93c009dd9ad1974aeb3e2c8d4
ILINSK <alexanderilinskiy2002@gmail.com>
2024-05-16 01:44:53 +0300

    /

--- random_game_service/app/main.py
+++ random_game_service/app/main.py
@@ -1,34 +1,75 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, status
 import requests
+from keycloak import KeycloakOpenID
+from fastapi import FastAPI, HTTPException, status, Depends, Form, Header
 
 app = FastAPI()
 
 FREE_TO_GAME_API_URL = "https://www.freetogame.com/api"
 
+KEYCLOAK_URL = "http://keycloak:8080/"
+KEYCLOAK_CLIENT_ID = "testClient"
+KEYCLOAK_REALM = "testRealm"
+KEYCLOAK_CLIENT_SECRET = "**********"
+
+user_token = ""
+keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
+                                  client_id=KEYCLOAK_CLIENT_ID,
+                                  realm_name=KEYCLOAK_REALM,
+                                  client_secret_key=KEYCLOAK_CLIENT_SECRET)
+
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def service_alive():
     return {'message': 'Service alive'}
 
+@app.post("/get_token")
+async def get_token(username: str = Form(...), password: str = Form(...)):
+    try:
+        # Получение токена
+        token = keycloak_openid.token(grant_type=["password"],
+                                      username=username,
+                                      password=password)
+        global user_token
+        user_token = token
+        return token
+    except Exception as e:
+        print(e)  # Логирование для диагностики
+        raise HTTPException(status_code=400, detail="Не удалось получить токен")
+
+def check_user_roles(token):
+    try:
+        token_info = keycloak_openid.introspect(token)
+        if "test" not in token_info["realm_access"]["roles"]:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return token_info
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
+
 @app.get("/game/{game_id}")
-async def get_game_by_id(game_id: int):
-    url = f"{FREE_TO_GAME_API_URL}/game?id={game_id}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data
+async def get_game_by_id(game_id: int, token: str = Header(...)):
+    if check_user_roles(token):
+        url = f"{FREE_TO_GAME_API_URL}/game?id={game_id}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            raise HTTPException(status_code=400, detail="Error retrieving game data")
     else:
-        raise HTTPException(status_code=400, detail="Error retrieving game data")
+        return "Wrong JWT Token"
 
 @app.get("/games")
-async def get_games():
-    url = f"{FREE_TO_GAME_API_URL}/games"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data
+async def get_games(token: str = Header(...)):
+    if check_user_roles(token):
+        url = f"{FREE_TO_GAME_API_URL}/games"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            raise HTTPException(status_code=400, detail="Error retrieving games data")
     else:
-        raise HTTPException(status_code=400, detail="Error retrieving games data")
+        return "Wrong JWT Token"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
